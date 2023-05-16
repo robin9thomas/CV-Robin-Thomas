cd17c62c084f72ce80becb7503f5795bc3ef4c36
robin9thomas <robin.19thomas@gmail.com>
2023-05-16 15:33:59 +0200

    Ajout défilement horizontal

---
+++ script.js
@@ -0,0 +1,21 @@
+
+function scrolltoLeft() {
+  var largeurEcran = window.innerWidth;
+  console.log(largeurEcran);
+  const container = document.querySelector('.container');
+  const scrollAmount = container.scrollLeft - largeurEcran; 
+  container.scrollTo({
+    left: scrollAmount,
+    behavior: 'smooth'
+  });
+}
+
+function scrollRight() {
+  var largeurEcran = window.innerWidth;
+  const container = document.querySelector('.container');
+  const scrollAmount = container.scrollLeft + largeurEcran; 
+  container.scrollTo({
+    left: scrollAmount,
+    behavior: 'smooth' 
+  });
+}
